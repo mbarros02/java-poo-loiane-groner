1813a933020ae3ec2d657d468159ba3a0c9f4b28
Marcello Barros <barroscello123@gmail.com>
2025-08-19 00:28:31 -0300

    Classe e métodos criados na aula 27.

---
+++ src/Aula27/Carro.java
@@ -0,0 +1,24 @@
+package Aula27;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+    void exibirAutonomia() {
+        System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel);
+    }
+
+    double obterAutonomia() {
+        System.out.println("Método obter autonomia foi chamado!");
+        return capCombustivel * consumoCombustivel;
+    }
+
+    double calcularCombustivel(double km) {
+        double qtdeCombustivel = km / consumoCombustivel;
+        return qtdeCombustivel;
+    }
+}
---
+++ src/Aula27/TesteCarro.java
@@ -0,0 +1,24 @@
+package Aula27;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+
+        Carro car = new Carro();
+        car.marca = "Fiat";
+        car.modelo = "Ducato";
+        car.numPassageiros = 10;
+        car.capCombustivel = 100;
+        car.consumoCombustivel = 0.2;
+
+        System.out.println(car.marca);
+        System.out.println(car.modelo);
+
+        car.exibirAutonomia();
+
+        double autonomia = car.obterAutonomia();
+
+        System.out.println("A autonomia do carro é de " + car.obterAutonomia());
+
+        double qtdeCombustivel = car.calcularCombustivel(10.0);
+    }
+}
